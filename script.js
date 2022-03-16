6dd993de8e7f1e4690289d2956627f896228ce7e
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-16 15:36:58 +0100

    creata funzione per celle

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-
-/*creare una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
-
-/*BONUS:
-L'utente indica un livello di difficoltà, in base al livello scelto la griglia conterrà un range diverso:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49*/
-
-
-
 /*
 Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 I numeri nella lista delle bombe non possono essere duplicati.
@@ -19,6 +7,10 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 BONUS:
 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+3- L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
 */
 
 let richiestaUtente =parseInt(prompt("richiesta difficolta 1,2,3"));
@@ -50,7 +42,7 @@ const totalCells = colums * rows;
 
 for(let i = 0; i<totalCells;i++){
 
-    const cell = document.createElement('div');
+    const cell = createCell();
     grid.appendChild(cell);
 
     if(richiestaUtente==1){
@@ -65,4 +57,11 @@ for(let i = 0; i<totalCells;i++){
     cell.addEventListener('click',function(event){
         cell.classList.toggle("bg-cyan")
     })
+}
+
+
+function createCell() {
+    const item = document.createElement('div');
+    item.classList.add('cell');
+    return item;
 }
